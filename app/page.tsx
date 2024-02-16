a69c3a8f143c1c4b62783849efa6a8127ec6200a
Keyvan <skhprogramming@gmail.com>
2024-02-16 19:05:01 +0330

    Added new icon & prettier settings

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
-import { Room } from "./Room";
-
-
 export default function Page() {
   return (
-    <Room>
-      <h1 className='font-xl'>figma</h1>
-    </Room>
+      <h1 className='text-5xl text-white'>Figma Clone</h1>
   );
 }
