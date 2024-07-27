d928a3783c5e63e4a39a889bdad5829e9a136251
Keyvan <skhprogramming@gmail.com>
2024-07-27 15:30:55 +0330

    add: left sidebar layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,19 @@
 'use client';
 
+import LeftSidebar from '@/components/LeftSidebar';
 import Live from '@/components/Live';
 import Navbar from '@/components/Navbar';
+import RightSidebar from '@/components/RightSidebar';
 
 export default function Page() {
   return (
     <main className="h-screen overflow-hidden">
       <Navbar />
+
       <section className="flex flex-row h-full">
+        <LeftSidebar />
         <Live />
+        <RightSidebar />
       </section>
     </main>
   );
---
+++ components/LeftSidebar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LeftSidebar = () => {
+  return (
+    <section className="flex flex-col border-t border-primary-grey-200 bg-primary-black text-primary-grey-300 min-2-[227px] sticky left-0 h-full max-sm:hidden select-none overflow-y-auto pb-20">
+      <div>LeftSidebar</div>
+    </section>
+  );
+};
+
+export default LeftSidebar;
