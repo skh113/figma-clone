eee479fd78db7e3c18b8b8ad75339f9263a9f7e6
Keyvan <skhprogramming@gmail.com>
2024-09-17 18:27:09 +0330

    fix: made the loader image eagerly

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,15 +1,18 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 const Loader = () => (
-  <div className='flex h-screen w-screen flex-col items-center justify-center gap-2'>
+  <div className="flex h-screen w-screen flex-col items-center justify-center gap-2">
     <Image
-      src='/assets/loader.gif'
-      alt='loader'
+      src="/assets/loader.gif"
+      alt="loader"
       width={100}
       height={100}
-      className='object-contain'
+      property
+      quality={100}
+      loading="eager"
+      className="object-contain"
     />
-    <p className='text-sm font-bold text-primary-grey-300'>Loading...</p>
+    <p className="text-sm font-bold text-primary-grey-300">Loading...</p>
   </div>
 );
 
